2c65e5cf39ceb6d4cf226cc5f27f3da7541159f1
wotjr1649 <wotjr980321@naver.com>
2024-05-29 07:21:11 +0900

    14

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ from fastapi.exception_handlers import (
     request_validation_exception_handler,
 )
 from fastapi.exceptions import RequestValidationError
+from routes import host_routers, hosting_routers, login_routers, sports_crawl_routers
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
@@ -34,6 +35,13 @@ async def custom_http_exception_handler(request, exc):
 async def validation_exception_handler(request, exc):
     print(f"OMG! The client sent invalid data!: {exc}")
     return await request_validation_exception_handler(request, exc)
+
+
+# 라우터 등록
+app.include_router(login_routers, prefix="/login")
+app.include_router(host_routers, prefix="/host")
+app.include_router(hosting_routers, prefix="/hosting")
+app.include_router(sports_crawl_routers, prefix="/schedule")
 
 
 @app.get("/")
