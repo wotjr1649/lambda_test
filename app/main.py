fed8bcfda14f27e48c133cc87a63991890a6696f
wotjr1649 <wotjr980321@naver.com>
2024-05-29 06:42:56 +0900

    12

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 # app/main.py
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(openapi_prefix="/api/v1")
 
 @app.get("/")
 async def root():
     return {"message": "Hello, World!"}
+@app.get("/test")
+async def root():
+    return {"message": "Hello, test!"}
