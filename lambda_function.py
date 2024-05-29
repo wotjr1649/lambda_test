db638fd27bf19f9972be827a0582413ce96c1730
wotjr1649 <wotjr980321@naver.com>
2024-05-29 22:36:00 +0900

    99

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,20 @@
-import mangum
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from mangum import Mangum
 
 app = FastAPI()
+
+origins = [
+    "*",  # 모든 도메인 허용 (보안상의 이유로 필요에 따라 제한)
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
@@ -9,4 +22,5 @@ def read_root():
     return {"Hello": "World"}
 
 
-handler = mangum.Mangum(app)
+# Mangum 핸들러 생성
+handler = Mangum(app)
